b575595c3a031f4a10a61a0f885932bc18671f8c
Robert Maidla <robertmaidla@gmail.com>
2019-01-30 15:02:47 +0200

    First commit

---
+++ routes/index.js
@@ -0,0 +1,121 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var mid = require('../middleware');
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+// GET /register
+router.get('/register', mid.loggedOut, function(req, res, next) {
+  return res.render('register', { title: 'Sign Up' });
+});
+
+// POST /register
+router.post('/register', function(req, res, next) {
+  // check for input fields
+  if (req.body.email &&
+      req.body.name &&
+      req.body.favoriteBook &&
+      req.body.password &&
+      req.body.confirmPassword) {
+      
+      // check if passwords match
+      if (req.body.password !== req.body.confirmPassword) {
+        // error handling
+        var err = new Error('Passwords do not match.');
+        err.statud = 400;
+        return next(err);
+      }
+
+      // create object with form input
+      var userData = {
+        email: req.body.email,
+        name: req.body.name,
+        favoriteBook: req.body.favoriteBook,
+        password: req.body.password
+      };
+
+      // use schema's 'create' method to insert document into Mongo
+      User.create(userData, function(error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          req.session.userId = user._id;
+          return res.redirect('/profile');
+        }
+      });
+
+    } else {
+      // error handling
+      var err = new Error('All fields required.');
+      err.statud = 400;
+      return next(err);
+    }
+});
+
+// GET /login
+router.get('/login', mid.loggedOut, function(req, res, next) {
+  return res.render('login', {title: 'Log In'});
+});
+
+// POST /login
+router.post('/login', function(req, res, next) {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
+// GET /profile
+router.get('/profile', mid.requiresLogIn, function(req, res, next) {
+  User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        return res.render('profile', { title: 'Profile', name: user.name, 
+                                      favorite: user.favoriteBook});
+      }
+    });
+});
+
+// GET /logout
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+module.exports = router;
